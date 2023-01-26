341b0472387dd0d0f27d47e1c41643047c947e57
Kenneth Kimani <kimanim68@gmail.com>
2023-01-26 16:33:45 +0300

    title-body

--- doawk/src/components/ListItem.js
+++ doawk/src/components/ListItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getTitle = (note) => {
+    const title = note.body.split('\n')[0]
+    return title.length > 20 ? title.substring(0, 20) + '...' : title
+}
+
+
+
+
 const ListItem = ({note}) => {
     return (
         <Link to={`/note/${note.id}`}>
             <div className="notes-list-item">
-            <h2>{note.body}</h2>
+            <h2>{getTitle(note)}</h2>
             </div>
         </Link>
     )
