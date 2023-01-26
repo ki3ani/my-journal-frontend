b52e6c9d3af58f8aaee185004a05a16d43347baa
Kenneth Kimani <kimanim68@gmail.com>
2023-01-26 16:42:57 +0300

    list-item-date

--- doawk/src/components/ListItem.js
+++ doawk/src/components/ListItem.js
@@ -6,6 +6,11 @@ let getTitle = (note) => {
     return title.length > 20 ? title.substring(0, 20) + '...' : title
 }
 
+let getDate = (note) => {
+    const date = new Date(note.updated)
+    return date.toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})
+}
+
 
 
 
@@ -14,6 +19,7 @@ const ListItem = ({note}) => {
         <Link to={`/note/${note.id}`}>
             <div className="notes-list-item">
             <h2>{getTitle(note)}</h2>
+            <p><span>{getDate(note)}</span></p>
             </div>
         </Link>
     )
